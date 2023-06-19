01997f8ce6251081561c574ea3a5c9511085378f
vrfjr <vagnerrfjr@Gmail.com>
2023-06-19 17:03:48 -0300

    Adicionar arquivos de projeto.

---
+++ MeuCadastro/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MeuCadastro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuCadastro.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Vagner";
+            home.Email = "vagner@gmail.com";  
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
